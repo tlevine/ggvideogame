a72bab81eedb34c4ac391c60f5b64a3af9224aa9
Thomas Levine <_@thomaslevine.com>
2015-05-15 16:26:34 +0000

    -

---
+++ ggvideogame.py
@@ -0,0 +1,8 @@
+def ggvideogame(data, x = None, y = None, hue = None, brightness = None,
+                stick1 = None, stick2 = None, facet = None):
+    '''
+    :param list data: Dataset, a list of dictionaries
+
+    Everything else is either a column name (a key in the dictionaries)
+    or None.
+    '''
