6162bc1b03ca8cfb062698e8dad2fb5f53af1195
Thomas Levine <_@thomaslevine.com>
2015-05-15 17:51:55 +0000

    -

--- ggvideogame.py
+++ ggvideogame.py
@@ -1,15 +1,12 @@
 import itertools
 import collections
 
-DEFAULT_AESTHETICS = {
+import pandas
+
+DEFAULTS = {
     'x': 0, 'y': 0,
     'hue': None,
     'brightness': 0.5,
-}
-MAX_GROUPS = {
-    'stick1': 100,
-    'stick2': 100,
-    'panel': 4,
 }
 LIMITS = {
     'x': 30,
@@ -30,21 +27,41 @@ def ggvideogame(df, x = None, y = None, hue = None, brightness = None,
     or None.
     '''
 
+    aesthetics = {
+        'x': x,
+        'y': y,
+        'hue': hue,
+        'brightness': brightness,
+    }
+
     # Determine limits.
-    def frame(panel_value, stick1_value, stick2_value, limits = LIMITS):
+    def frame(panel_value, stick1_value, stick2_value,
+              limits = LIMITS, defaults = DEFAULTS):
         '''
         Subset the particular frame. Use None to get everything.
         '''
         # Select the data for a particular facet.
-        selector = True
+        selector = pandas.Series(list(itertools.repeat(True, df.shape[0])))
         factor_levels = [(panel, panel_value), (stick1, stick1_value), (stick2, stick2_value)]
         for factor, level in factor_levels:
             if level:
                 selector = selector & (df[factor] == level)
         
-        return {colname: scale(df[colname],
-                               df[selector][colname],
-                               LIMITS[colname]) for colname in LIMITS.keys()}
+        # Defaults
+        df_subset = pandas.DataFrame()
+        for aesthetic in limits.keys():
+            default_column = list(itertools.repeat(defaults[aesthetic], selector.sum()))
+            df_subset[aesthetic] = pandas.Series(default_column)
+
+        for aesthetic in limits.keys():
+            if aesthetics[aesthetic]:
+                df_subset[aesthetic] = scale(df[aesthetics[aesthetic]],
+                                             df[selector][aesthetics[aesthetic]],
+                                             limits[aesthetic])
+
+        return df_subset
+    
+    return frame
 
 def scale(column, subcolumn, n):
     normalized = (subcolumn - column.min()) / (column.max() - column.min())
