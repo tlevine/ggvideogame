dd13b5a40f85e23628ef276dee4cf112300ee36d
Thomas Levine <_@thomaslevine.com>
2015-05-15 16:52:15 +0000

    -

--- ggvideogame.py
+++ ggvideogame.py
@@ -8,7 +8,7 @@ DEFAULT_AESTHETICS = {
 }
 MAX_GROUPS = {
     'stick1': 50,
-    'stick2': 50,
+    'stick2': 100,
     'panel': 4,
 }
 
@@ -24,11 +24,12 @@ def ggvideogame(data, x = None, y = None, hue = None, brightness = None,
 def build_canvas(data, **kwargs):
     canvas = collections.defaultdict(list)
     for facet in ['stick1', 'stick2', 'panel']:
-        for group in groups(data, facet, kwargs[facet]):
+        for group in groups(data, kwargs[facet], MAX_GROUPS[facet]):
             local_canvas = {}
-            local_data = subset(data, kwargs[facet], group)
+            local_data = list(subset(data, kwargs[facet], group))
             for aesthetic in ['x', 'y', 'hue', 'brightness']:
-                local_canvas[aesthetic] = column(local_data, kwargs[aesthetic])
+                default = DEFAULT_AESTHETICS[aesthetic]
+                local_canvas[aesthetic] = column(local_data, kwargs[aesthetic], default)
 
             canvas[facet].append(local_canvas)
     return canvas
@@ -38,18 +39,18 @@ def build_canvas(data, **kwargs):
 def subset(data, key, value):
     return (row for row in data if row[key] == value)
 
-def groups(data, facet, key, max_groups = MAX_GROUPS):
+def groups(data, key, max_groups):
     'Get groups for faceting.'
     groups = set(row[key] for row in data)
-    if len(groups) > MAX_GROUPS[facet]:
+    if len(groups) > max_groups:
         raise ValueError('Too many groups for the %s facet' % key)
     return sorted(groups)
 
-def column(data, aesthetic, key, defaults = DEFAULT_AESTHETICS):
+def column(data, key, default):
     if key:
         return (row[key] for row in data)
     else:
-        return itertools.repeat(defaults[key], len(data))
+        return itertools.repeat(default, len(data))
 
 
 # data = list(csv.DictReader(open('../maluku/data/voyages.csv')))
